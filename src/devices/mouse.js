8b018557c76f19fcbfea8db349d2ce6b475200df
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-10-17 16:09:15 +0200

    Caching supportsEventsConstructor result

--- src/devices/mouse.js
+++ src/devices/mouse.js
@@ -1,62 +1,49 @@
 function Mouse() {
 
-		var utils = require('../utils.js');
+  var utils = require('../utils.js');
 
-    this.click = function click(elt) {
-    		var dispatched=this.dispatch('mousedown', elt);
-    		if(this.dispatch('mouseup', elt)&&dispatched) {
-        	return this.dispatch('click', elt);
-        }
-        return false;
-    };
+  this.click = function click(elt) {
+    var dispatched=this.dispatch('mousedown', elt);
+    if(this.dispatch('mouseup', elt)&&dispatched) {
+      return this.dispatch('click', elt);
+    }
+    return false;
+  };
 
-    this._click = function click(elt) {
-        return this.dispatch('click', elt);
-    };
+  this._click = function click(elt) {
+    return this.dispatch('click', elt);
+  };
 
   /**
-   * Dispatches a mouse event to the DOM element behind the provided selector.
-   *
-   * @param  String  type      Type of event to dispatch
-   * @param  DOMElement  elt  A DOMElement to click
-   * @return Boolean
-   */
+  * Dispatches a mouse event to the DOM element behind the provided selector.
+  *
+  * @param  String  type      Type of event to dispatch
+  * @param  DOMElement  elt  A DOMElement to click
+  * @return Boolean
+  */
   this.dispatch = function dispatch(type, elt) {
-      var event = this.supportsEventConstructors() ?
-      	this.createEvent(type, elt) :
-      	this.legacyCreateEvent(type, elt);
-        return elt.dispatchEvent(event);
-    };
+    var event = utils.supportsEventConstructors ?
+      this.createEvent(type, elt) :
+      this.legacyCreateEvent(type, elt);
+    return elt.dispatchEvent(event);
+  };
 
-    this.createEvent = function createEvent(type, elt) {
-        return new MouseEvent(type, {
-            'view': window,
-            'bubbles': true,
-            'cancelable': true
-        });
-    };
+  this.createEvent = function createEvent(type, elt) {
+    return new MouseEvent(type, {
+      'view': window,
+      'bubbles': true,
+      'cancelable': true
+    });
+  };
 
-    this.legacyCreateEvent = function legacyCreateEvent(type, elt) {
-        var evt = document.createEvent("MouseEvents");
-        utils.setEventCoords(evt, elt);
-        evt.initMouseEvent(type, true, true, window, 1, 1, 1,
-        	evt.pageX, evt.pageY, false, false, false, false, 0, elt);
-        return evt;
-    };
+  this.legacyCreateEvent = function legacyCreateEvent(type, elt) {
+    var evt = document.createEvent("MouseEvents");
+    utils.setEventCoords(evt, elt);
+    evt.initMouseEvent(type, true, true, window, 1, 1, 1,
+    evt.pageX, evt.pageY, false, false, false, false, 0, elt);
+    return evt;
+  };
 
-    this.supportsEventConstructors = function supportsEventConstructors() {
-        try {
-            if (new Event('submit', { bubbles: false }).bubbles !== false) {
-                return false;
-            } else if (new Event('submit', { bubbles: true }).bubbles !== true) {
-                return false
-            } else {
-                return true;
-            }
-        } catch (e) {
-            return false;
-        }
-    };
 };
 
 module.exports = new Mouse();
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 module.exports={
-	'setEventCoords': function(event, element) {
+	setEventCoords: function(event, element) {
+	  // FIXME : should calculate pageX/Y, clientX/Y, screenX/Y and use setEventProperty
 		try {
 			var rect = elt.getBoundingClientRect();
       event.pageX = Math.floor((rect.left + rect.right) / 2);
@@ -8,5 +9,24 @@ module.exports={
       event.pageX = 1;
       event.pageY = 1;
     }
-	}
+	},
+	setEventProperty: function(event, property, value) {
+		try {
+				Object.defineProperty(event, property, {
+					get : function() {
+						return value;
+					}
+				});
+			} catch(e) {
+				event[property]=value;
+			}
+	},
+  supportsEventConstructors : ((function () {
+    try {
+      return new Event('submit', { bubbles: false }).bubbles === false
+        && new Event('submit', { bubbles: true }).bubbles === true;
+    } catch (e) {
+      return false;
+    }
+  })())
 };
