4ba4ba05f9ae774479e441d27d7e8556b1c55147
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-10-17 16:32:52 +0200

    Tactile device refactoring (constructor pattern)

--- src/devices/tactile.js
+++ src/devices/tactile.js
@@ -1,52 +1,49 @@
-var Tactile=(function () {
+function Tactile() {
 
 	// Neeed mouse to perform click
 	var mouse = require('./mouse.js');
 	var utils = require('../utils.js');
 
 	// Detect the tactile device
-	function isConnected () {
+	this.isConnected = function () {
+	  return true;
 		return !!('ontouchstart' in window);
-	}
+	};
 
 	// Trigger tactile events
-	function tactile (element,options) {
+	this.dispatch = function (element,options) {
 		options=options||{};
-		var event = document.createEvent('UIEvent');
-		event.initUIEvent(options.type,
-			'false' === options.canBubble ? false : true,
-			'false' === options.cancelable ? false : true,
-			options.view||window,
-			options.detail||1);
-		event.view = options.view||window;
-		event.altKey = !!options.altKey;
-		event.ctrlKey = !!options.ctrlKey;
-		event.shiftKey = !!options.shiftKey;
-		event.metaKey = !!options.metaKey;
-	  utils.setEventCoords(event, element);
+		var event
+		if(utils.supportsEventConstructors) {
+		  event = new UIEvent(options.type, {
+        'view': options.view||window,
+        'bubbles': 'false' === options.canBubble ? false : true,
+        'cancelable': 'false' === options.cancelable ? false : true
+      });
+    } else {
+		  event = document.createEvent('UIEvent');
+		  event.initUIEvent(options.type,
+			  'false' === options.canBubble ? false : true,
+			  'false' === options.cancelable ? false : true,
+			  options.view||window,
+			  options.detail||1);
+    }
 		return element.dispatchEvent(event);
-	}
+	};
 
 	// Touch the screen and release
-	function touch (element,options) {
+	this.touch = function (element,options) {
 		var dispatched;
 		options=options||{};
 		options.type='touchstart';
-		dispatched=tactile(element, options);
+		dispatched=this.dispatch(element, options);
 		options.type='touchend';
-		if(tactile(element, options)&&dispatched) {
+		if(this.dispatch(element, options)&&dispatched) {
 			return mouse._click(element);
 		}
 		return false;
-	}
-
-
-	return {
-		isConnected : isConnected,
-		tactile : tactile,
-		touch : touch
 	};
 
-})();
+}
 
-module.exports = Tactile;
+module.exports = new Tactile();
