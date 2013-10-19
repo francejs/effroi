f384947cbea7ff9ee6ad9cbfab230e288a4ae95e
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-10-19 11:05:08 +0200

    Road to multi-touch tactile events

--- src/devices/tactile.js
+++ src/devices/tactile.js
@@ -9,39 +9,64 @@ function Tactile() {
     return !!('ontouchstart' in window);
   };
 
-  // Trigger tactile events
-  this.dispatch = function (element,options) {
-    options=options||{};
-    var event
-    if(utils.supportsEventConstructors) {
-      event = new UIEvent(options.type, {
-        'view': options.view||window,
-        'bubbles': 'false' === options.canBubble ? false : true,
-        'cancelable': 'false' === options.cancelable ? false : true
-      });
-    } else {
-      event = document.createEvent('UIEvent');
-      event.initUIEvent(options.type,
-        'false' === options.canBubble ? false : true,
-        'false' === options.cancelable ? false : true,
-        options.view||window,
-        options.detail||1);
-    }
-    return element.dispatchEvent(event);
-  };
-
   // Touch the screen and release
   this.touch = function (element,options) {
     var dispatched;
-    options=options||{};
-    options.type='touchstart';
-    dispatched=this.dispatch(element, options);
-    options.type='touchend';
+    options = options || {};
+    options.type = 'touchstart';
+    dispatched = this.dispatch(element, options);
+    options.type = 'touchend';
     if(this.dispatch(element, options)&&dispatched) {
-      options.type='click';
+      options.type = 'click';
       return mouse.dispatch(element, options);
     }
     return false;
+  };
+
+  // Trigger tactile events
+  this.dispatch = function (element,options) {
+    var event = document.createEvent('UIEvent'), fakeEvent={};
+    options = options || {};
+    options.canBubble = ('false' === options.canBubble ? false : true);
+    options.cancelable = ('false' === options.cancelable ? false : true);
+    options.view = options.view || window;
+    options.detail = options.detail || 1,
+    options.altKey = !!options.altKey;
+    options.ctrlKey = !!options.ctrlKey;
+    options.shiftKey = !!options.shiftKey;
+    options.metaKey = !!options.metaKey;
+    options.changedTouches = options.changedTouches || [];
+    options.touches = options.touches | [];
+    options.scale = options.scale || 1.0;
+    options.rotation = options.rotation || 0.0;
+    // Safari, Firefox: must use initTouchEvent.
+    if ("function" === typeof event.initTouchEvent) {
+        utils.setEventCoords(fakeEvent, element);
+        event.initTouchEvent(options.type,
+          options.canBubble, options.cancelable,
+          options.view, options.detail,
+          fakeEvent.screenX, fakeEvent.screenY,
+          fakeEvent.clientX, fakeEvent.clientY,
+          options.ctrlKey, options.altKey,
+          options.shiftKey, options.metaKey,
+          options.touches, options.targetTouches, options.changedTouches,
+          options.scale, options.rotation);
+        utils.setEventCoords(event, element);
+    } else {
+        event.initUIEvent(options.type,
+          options.canBubble, options.cancelable,
+          options.view, options.detail);
+        utils.setEventProperty(event, 'altKey', options.altKey);
+        utils.setEventProperty(event, 'ctrlKey', options.ctrlKey);
+        utils.setEventProperty(event, 'shiftKey', options.shiftKey);
+        utils.setEventProperty(event, 'metaKey', options.metaKey);
+        utils.setEventProperty(event, 'changedTouches', options.changedTouches);
+        utils.setEventProperty(event, 'touches', options.touches);
+        utils.setEventProperty(event, 'scale', options.scale);
+        utils.setEventProperty(event, 'rotation', options.rotation);
+        utils.setEventCoords(event, element);
+    }
+    return element.dispatchEvent(event);
   };
 
 }
